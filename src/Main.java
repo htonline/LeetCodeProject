4b0bd1c593d77a639dde7a8f85f9b3be8e964dd5
htonline <986344867@qq.com>
2023-04-11 13:21:24 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Zuohaitao
+ * @date ${YEAR}-${MONTH}-${DAY} ${TIME}
+ * @describe
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
